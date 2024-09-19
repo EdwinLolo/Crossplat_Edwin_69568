0b9739d1094eaea194b130d2babc4c8cf52e116a
EdwinLolo <edwinfedora@gmail.com>
2024-09-19 20:18:26 +0700

    modul

--- EdwinFedoraLolo_69568/EdwinFedoraLolo_69568/App.js
+++ EdwinFedoraLolo_69568/EdwinFedoraLolo_69568/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Image, ScrollView } from "react-native";
+import userData from "./data.json";
+import styles from "./appStyle.styles.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      {userData.map((users) => {
+        return (
+          <View style={styles.container} key={users.name}>
+            <View style={styles.card}>
+              <Image style={styles.avatar} source={{ uri: users.photo_url }} />
+              <View style={styles.boldText}>
+                <Text style={styles.boldText}>{users.name}</Text>
+                <Text>{users.email}</Text>
+              </View>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
