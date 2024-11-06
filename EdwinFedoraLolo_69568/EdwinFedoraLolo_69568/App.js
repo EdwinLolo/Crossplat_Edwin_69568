99b0badc244b609f78597cc3996301653bc12e5a
EdwinLolo <edwinfedora@gmail.com>
2024-11-06 11:07:55 +0700

    oke

--- EdwinFedoraLolo_69568/EdwinFedoraLolo_69568/App.js
+++ EdwinFedoraLolo_69568/EdwinFedoraLolo_69568/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, View, Text, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [uri, setUri] = useState("");
+
+  const openImagePicker = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setUri(result.uri);
+    }
+  };
+
+  const handleCameraLaunch = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status === "granted") {
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setUri(result.uri);
+      }
+    } else {
+      alert("Camera permission is required to use the camera.");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Camera and Gallery Access</Text>
+      <Button title="Open Gallery" onPress={openImagePicker} />
+      <Button title="Open Camera" onPress={handleCameraLaunch} />
+      {uri ? (
+        <Image
+          source={{ uri }}
+          style={{ width: 200, height: 200, marginTop: 20 }}
+        />
+      ) : null}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
